4160dc4161a1bda5d2fab6d8226b6cd52c12cb71
jaredhall18 <jaredhall18@gmail.com>
2021-11-12 13:18:34 -0700

    first 3 tests in article test created and passing.

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -5,15 +5,44 @@ import userEvent from '@testing-library/user-event';
 import MutationObserver from 'mutationobserver-shim';
 
 import Article from './Article';
+import { render, screen } from '@testing-library/react';
+
+const testArticle = {
+    id:"1",
+    headline: "This is an article",
+    author: "Jared Hall",
+    image: 123,
+    createdOn: "today",
+    summary: "Test summary",
+    body: "Test body"
+};
+const testArticle2 = {
+    id:"1",
+    headline: "This is an article",
+    author: "",
+    image: 123,
+    createdOn: "today",
+    summary: "Test summary",
+    body: "Test body"
+};
 
 test('renders component without errors', ()=> {
+    render(<Article article={testArticle}/>)
 });
 
-// test('renders headline, author from the article when passed in through props', ()=> {
-// });
+test('renders headline, author from the article when passed in through props', ()=> {
+    render(<Article article={testArticle}/>)
+    const headline= screen.queryByTestId(/headline/i);
+    const author= screen.queryByTestId(/author/i);
+    expect(headline).toBeInTheDocument();
+    expect(author).toBeInTheDocument();
+});
 
-// test('renders "Associated Press" when no author is given', ()=> {
-// });
+test('renders "Associated Press" when no author is given', ()=> {
+    render(<Article article={testArticle2} />);
+    const noAuthor= screen.queryByText(/By Associated Press/i);
+    expect(noAuthor).toBeInTheDocument();
+});
 
 // test('executes handleDelete when the delete button is pressed', ()=> {
 // });
